b433f004b7fe500ea29e0aaa55171857f6fa6bbc
Jakob Majnek <jakob.majnek@smartbuoy.eu>
2026-02-14 23:30:19 +0100

    feat: redesign hero2

--- src/types.d.ts
+++ src/types.d.ts
@@ -252,6 +252,7 @@ export interface Hero extends Omit<Headline, 'classes'>, Omit<Widget, 'isDark' |
   content?: string;
   actions?: string | CallToAction[];
   image?: string | unknown;
+  imageObjectPositionDesktop?: string;
 }
 
 export interface Team extends Omit<Headline, 'classes'>, Widget {
